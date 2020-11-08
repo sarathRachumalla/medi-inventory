de1b3d763f2323b06626b261cdb2d2cfbcea631c
Sarath02 <sarath.kumar@neutrinos.co>
2020-11-08 09:49:38 +0530

    inital

---
+++ app/src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    "name": "dev",
+    "properties": {
+        "production": false,
+        "ssdURL": "http://localhost:8081/api/",
+        "tenantName": "neutrinos",
+        "appName": "medi-inventory",
+        "namespace": "com.neutrinos.medi-inventory",
+        "googleMapKey": "AIzaSyCSTnVwijjv0CFRA4MEeS-H6PAQc87LEoU",
+        "useDefaultExceptionUI": true,
+        "isIDSEnabled": "false",
+        "webAppMountpoint": "web",
+        "NGFORAGE_MOBILE_DRIVER": "INDEXED_DB"
+    }
+}
